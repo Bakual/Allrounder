899c8c990d2d80c203fa4e9e119a5b17b33ef537
Bakual <werbemails@bakual.ch>
2013-03-11 20:44:26 +0100

    cosmetic

--- allrounder-3/index.php
+++ allrounder-3/index.php
@@ -1,6 +1,6 @@
 <?php
 defined( '_JEXEC' ) or die( 'Access to this location is RESTRICTED.' );
-echo '<?xml version="1.0" encoding="utf-8"?'.'>'; 
+echo '<?xml version="1.0" encoding="utf-8"?'.'>';
 ?>
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="<?php echo $this->language; ?>" lang="<?php echo $this->language; ?>" >
