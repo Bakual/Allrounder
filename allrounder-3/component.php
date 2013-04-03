8a6b2910204e83265490811296a7a4e1f6eaa9b5
Bakual <werbemails@bakual.ch>
2013-04-03 15:54:13 +0200

    Adjusting component.php

--- allrounder-3/component.php
+++ allrounder-3/component.php
@@ -1,18 +1,24 @@
 <?php
 // no direct access
-defined( '_JEXEC' ) or die( 'Restricted access' );
+defined('_JEXEC') or die('Restricted access');
+
+$tpl	= JFactory::getApplication()->getTemplate(true);
+$path	= $this->baseurl.'/templates/'.$tpl->template;
 ?>
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="<?php echo $this->language; ?>" lang="<?php echo $this->language; ?>" dir="<?php echo $this->direction; ?>">
 <head>
+	<meta http-equiv="content-type" content="text/html; charset=utf-8" />
+	<meta name="designer" content="Juergen Koller - http://www.lernvid.com" />
+	<meta name="licence" content="Creative Commons 3.0" />
+	<link href="<?php echo $path; ?>/favicon.ico" rel="shortcut icon" type="image/x-icon" />
 	<jdoc:include type="head" />
-	<link href="<?php echo $this->baseurl ?>/templates/<?php echo $this->template ?>/css/template.css" rel="stylesheet" type="text/css" media="all" />
-	<link href="<?php echo $this->baseurl ?>/templates/<?php echo $this->template ?>/css/joomla.css" rel="stylesheet" type="text/css" media="all" />
-	<link href="<?php echo $this->baseurl ?>/templates/<?php echo $this->template ?>/css/colors.css" rel="stylesheet" type="text/css" media="all" />
-	<link href="<?php echo $this->baseurl ?>/templates/<?php echo $this->template ?>/css/print.css" rel="stylesheet" type="text/css" media="all" />
-	<link href="<?php echo $this->baseurl ?>/templates/<?php echo $this->template ?>/css/typo.css" rel="stylesheet" type="text/css" media="all" />
+	<link href="<?php echo $path; ?>/css/template<?php echo $this->params->get('useLESS') ? $tpl->id : ''; ?>.css" rel="stylesheet" type="text/css" media="all" />
+<!--	<link href="<?php // echo $this->baseurl ?>/templates/<?php // echo $this->template ?>/css/print.css" rel="stylesheet" type="text/css" media="all" /> -->
+	<?php // Load optional rtl Bootstrap css and Bootstrap bugfixes
+	JHtmlBootstrap::loadCss($includeMaincss = false, $this->direction); ?>
 </head>
-<body class="contentpane">
+<body class="contentpane modal">
 	<jdoc:include type="message" />
 	<jdoc:include type="component" />
 </body>
