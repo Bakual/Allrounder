891faa36c551c0c396508038bea805c717eec054
Bakual <werbemails@bakual.ch>
2013-04-09 14:06:25 +0200

    Fixing effects.js, adding detailer toggle

--- allrounder-3/js/effects.js
+++ allrounder-3/js/effects.js
@@ -1,5 +1,4 @@
 jQuery(document).ready(function($) {
-
 	// Mainmenu with suffix _menu
 	$('.module_menu ul.menu li a').hover(function(){
 		$(this).stop().animate({'padding-left':'20px'},{
@@ -13,17 +12,14 @@ jQuery(document).ready(function($) {
 		});
 	});
 
-
 	// Searchbox animation
 	$("#search").hide().show({},"swing"); 
-
 
 	// Go to top link
 	$('a#gotop').click(function(){
 	$('html, body').animate({scrollTop:0}, 1200); 
 		return false; 
 	});
-
 
 	// Tooltips
 	$('.lv-tooltip').tooltip({separator:'::',delay:200});
@@ -33,7 +29,7 @@ jQuery(document).ready(function($) {
 	$('.lv-tooltip-red').tooltip({className:'tip-red',separator:'::',delay:200});
 	$('.lv-tooltip-green').tooltip({className:'tip-green',separator:'::',delay:200});
 	$('.lv-tooltip-yellow').tooltip({className:'tip-yellow',separator:'::',delay:200});
-	
+
 	// Captions
 	$('.lv-caption').caption({opacity:0.6,animation:'fade',separator: '::',button:true});
 	$('.lv-caption-fade').caption({opacity:0.6,fixed:false,animation:'fade',separator: '::'});
@@ -43,17 +39,13 @@ jQuery(document).ready(function($) {
 	$('.lv-caption-zoom').caption({opacity:0.6,fixed:false,animation:'zoom',separator: '::'});
 	$('.lv-caption-racer').caption({opacity:0.6,fixed:false,animation:'racer',separator: '::'});
 	$('.lv-caption-move').caption({opacity:0.6,fixed:false,animation:'move',separator: '::'});
-	
 
-
-	// Atricle Details
-//	$('dl.article-info dd').hide();
-
-	$('article-info-term').click(function(){
+	// Article Details
+	$('.article-info dd').hide();
+	$('.article-info-term').click(function(){
 		$(this).stop();
 		$('.article-info dd').slideToggle("fast");
 	});
-
 
 	// Accordion
 	$('.hideme').hide();
@@ -72,16 +64,14 @@ jQuery(document).ready(function($) {
 	});
 	$('.lvacc-all').click(function(){
 		$('.hideme').stop().toggle();
-	
 	});
-	
+
 	// Toppanel
 //	$('#toppanel').hide();
 	$('a#toppanel-toggle').click(function(){
 		$('#toppanel').slideToggle("middle");
 		return true;
 	});
-
 
 	// Sidepanel
 //	$('#sidepanel').hide();
@@ -90,10 +80,8 @@ jQuery(document).ready(function($) {
 		return false;
 	});
 
-
 	// System Messages
-	$("div#system-message").fadeOut({
+	$("div#system-message div").fadeOut({
 		duration: 15000
 	});
-
 });
