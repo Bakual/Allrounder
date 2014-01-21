7e519ee7a35940d28c26b355bbd5aa100e9345f0
Bakual <werbemails@bakual.ch>
2014-01-21 14:55:03 +0100

    PHPCS

--- plg_lessallrounder/lessallrounder.php
+++ plg_lessallrounder/lessallrounder.php
@@ -1,28 +1,37 @@
 <?php
 /**
- * @package System Plugin - automatic Less compiler - for Allrounder template
- * @version 0.1
- * @author Thomas Hunziker
- * @copyright (C) 2013 - Thomas Hunziker
+ * @package     Lessallrounder
+ * @subpackage  Plugin
+ * @author      Thomas Hunziker <bakual@bakual.net>
+ * @copyright   (C) 2014 - Thomas Hunziker
+ * @license     GNU/GPLv3 http://www.gnu.org/licenses/gpl-3.0.html
+ *
  * Loosely based on https://github.com/ndeet/plg_system_less
- * @author Andreas Tasch 
- * @copyright (C) 2012 - Andreas Tasch
- * @license GNU/GPLv3 http://www.gnu.org/licenses/gpl-3.0.html
  **/
 
-// no direct access
-defined( '_JEXEC' ) or die();
+// No direct access
+defined('_JEXEC') or die();
 
 /**
  * Plugin compiles .less files on template style save.
  * Less compiler lessphp; see http://leafo.net/lessphp/
+ *
+ * @since  1.0
  */
-class plgSystemLessallrounder extends JPlugin
+class PlgSystemLessallrounder extends JPlugin
 {
 	/**
-	 * Compile .less files on change
+	 * Compile .less files on template style change
+	 *
+	 * @param   string  $context  Context of the data
+	 * @param   object  $table    Table object
+	 * @param   bool    $isNew    New entry or edit
+	 *
+	 * @return  void
+	 *
+	 * @since   1.0
 	 */
-	function onExtensionAfterSave($context, $table, $isNew)
+	public function onExtensionAfterSave($context, $table, $isNew)
 	{
 		if ($context != 'com_templates.style')
 		{
@@ -41,15 +50,16 @@ class plgSystemLessallrounder extends JPlugin
 			return;
 		}
 
-		//path to less file
+		// Path to less file
 		$client			= ($table->client_id) ? JPATH_ADMINISTRATOR : JPATH_SITE;
-		$templatePath	= $client.'/templates/'.$table->template;
-		$lessFile		= $templatePath.'/less/template.less';
-		$cssFile		= $templatePath.'/css/template'.$table->id.'.css';
-		//check if .less file exists and is readable
+		$templatePath	= $client . '/templates/' . $table->template;
+		$lessFile		= $templatePath . '/less/template.less';
+		$cssFile		= $templatePath . '/css/template' . $table->id . '.css';
+
+		// Check if .less file exists and is readable
 		if (is_readable($lessFile))
 		{
-			require_once('lessc.inc.php');
+			require_once 'lessc.inc.php';
 			$less = new lessc;
 
 			if ($table->params->get('cssCompress', 0))
@@ -67,6 +77,7 @@ class plgSystemLessallrounder extends JPlugin
 				$formatter->indentChar = "\t";
 				$less->setFormatter($formatter);
 			}
+
 			$params_array	= $table->params->toArray();
 
 			// Sanityzing params for LESS
@@ -75,7 +86,7 @@ class plgSystemLessallrounder extends JPlugin
 				// Adding quotes around variable so it's threated as string if a slash is in it.
 				if (strpos($value, '/') !== false)
 				{
-					$value	= '"'.$value.'"';
+					$value	= '"' . $value . '"';
 				}
 				// Quoting empty values as they break the compiler
 				if ($value == '')
@@ -86,30 +97,32 @@ class plgSystemLessallrounder extends JPlugin
 
 			// Adding templatepath to params
 			$basePath					= ($table->client_id) ? JURI::base(true) : JURI::root(true);
-			$params_array['basePath']	= '"'.$basePath.'/"';
+			$params_array['basePath']	= '"' . $basePath . '/"';
 
 			$less->setVariables($params_array);
 
-			$less->setImportDir(array($templatePath.'/less/'));
+			$less->setImportDir(array($templatePath . '/less/'));
 			jimport('joomla.filesystem.file');
 			$lessString	= JFile::read($lessFile);
 
 			// Check for custom files
-			if (is_readable($templatePath.'/less/custom.less'))
+			if (is_readable($templatePath . '/less/custom.less'))
 			{
-				$lessString	.= JFile::read($templatePath.'/less/custom.less');
-			}
-			if (is_readable($templatePath.'/css/custom.css'))
-			{
-				$lessString	.= JFile::read($templatePath.'/css/custom.css');
+				$lessString	.= JFile::read($templatePath . '/less/custom.less');
 			}
 
-			try {
-				$cssString = $less->compile($lessString);
-			} 
-			catch(Exception $e) 
+			if (is_readable($templatePath . '/css/custom.css'))
 			{
-				JError::raiseWarning(100, 'lessphp error: '.$e->getMessage());
+				$lessString	.= JFile::read($templatePath . '/css/custom.css');
+			}
+
+			try
+			{
+				$cssString = $less->compile($lessString);
+			}
+			catch (Exception $e)
+			{
+				JError::raiseWarning(100, 'lessphp error: ' . $e->getMessage());
 			}
 			JFile::write($cssFile, $cssString);
 
--- plg_lessallrounder/script.php
+++ plg_lessallrounder/script.php
@@ -1,23 +1,55 @@
-<?php defined('_JEXEC') or die('Restricted access');
+<?php 
+/**
+ * @package     Lessallrounder
+ * @subpackage  Plugin
+ * @author      Thomas Hunziker <bakual@bakual.net>
+ * @copyright   (C) 2014 - Thomas Hunziker
+ * @license     GNU/GPLv3 http://www.gnu.org/licenses/gpl-3.0.html
+ **/
 
-class plgSystemLessallrounderInstallerScript
-{ 
-	function update($parent)
-	{ 
+defined('_JEXEC') or die('Restricted access');
+
+/**
+ * Script to enable the plugin after installation/update
+ *
+ * @since  1.0
+ */
+class PlgSystemLessallrounderInstallerScript
+{
+	/**
+	 * Method to update the plugin
+	 *
+	 * @param   object  $parent  JInstallerAdapterPlugin class calling this method
+	 *
+	 * @return  void
+	 *
+	 * @since   1.0
+	 */
+	public function update($parent)
+	{
 		$this->install($parent);
 	}
 
-	function install($parent)
-	{ 
+	/**
+	 * Method to install the plugin
+	 *
+	 * @param   object  $parent  JInstallerAdapterPlugin class calling this method
+	 *
+	 * @return  void
+	 *
+	 * @since   1.0
+	 */
+	public function install($parent)
+	{
 		// Enable plugin
 		$db		= JFactory::getDbo();
 		$query	= $db->getQuery(true);
 		$query->update('#__extensions');
-		$query->set('`enabled` = 1');
-		$query->where('`element` = '.$db->quote('lessallrounder'));
-		$query->where('`type` = '.$db->quote('plugin'));
+		$query->set($db->quoteName('enabled') . ' = 1');
+		$query->where($db->quoteName('element') . ' = ' . $db->quote('lessallrounder'));
+		$query->where($db->quoteName('type') . ' = ' . $db->quote('plugin'));
 
 		$db->setQuery($query);
 		$db->execute();
-	} 
+	}
 }
