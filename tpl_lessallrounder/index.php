3f96f12054e0ffce7f6e9f50c3d454b67450395b
Bakual <werbemails@bakual.ch>
2014-03-20 22:38:50 +0100

    Load JQuery since we need it for the effects.js.

--- tpl_lessallrounder/index.php
+++ tpl_lessallrounder/index.php
@@ -15,6 +15,7 @@ $tpl	= $app->getTemplate(true);
 $path	= $this->baseurl . '/templates/' . $tpl->template;
 $params	= $tpl->params;
 
+JHtmlJquery::framework();
 JHtmlBootstrap::tooltip();
 ?>
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd"> 
