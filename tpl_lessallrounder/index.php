79ca903b9833d3be13fe8ccf933bdf56488fdf72
Bakual <werbemails@bakual.ch>
2014-03-22 20:47:22 +0100

    HTML5

--- tpl_lessallrounder/index.php
+++ tpl_lessallrounder/index.php
@@ -18,7 +18,7 @@ $params	= $tpl->params;
 JHtmlJquery::framework();
 JHtmlBootstrap::tooltip();
 ?>
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd"> 
+<!DOCTYPE html> 
 <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="<?php echo $this->language; ?>" lang="<?php echo $this->language; ?>" dir="<?php echo $this->direction; ?>">
 <head>
 	<meta http-equiv="content-type" content="text/html; charset=utf-8" />
