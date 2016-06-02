3216b9ec1a3da31846b33fc13837ec9c6d2c1226
Minei3oat <minei3oat@t-online.de>
2016-06-02 23:19:31 +0200

    centering nav-icons horizontal

--- tpl_lessallrounder/html/mod_menu/default_url.php
+++ tpl_lessallrounder/html/mod_menu/default_url.php
@@ -31,15 +31,15 @@ $flink = JFilterOutput::ampReplace(htmlspecialchars($flink));
 switch ($item->browserNav) :
 	default:
 	case 0:
-?><a <?php echo $class; ?>href="<?php echo $flink; ?>" <?php echo $title . $rel; ?>><?php echo $hackmenu . $linktype; ?></a><?php
+?><a <?php echo $class; ?>href="<?php echo $flink; ?>" <?php echo $title . $rel; ?>><?php echo $hackmenu; ?><div><?php echo $linktype; ?></div></a><?php
 		break;
 	case 1:
 		// _blank
-?><a <?php echo $class; ?>href="<?php echo $flink; ?>" target="_blank" <?php echo $title . $rel; ?>><?php echo $hackmenu . $linktype; ?></a><?php
+?><a <?php echo $class; ?>href="<?php echo $flink; ?>" target="_blank" <?php echo $title . $rel; ?>><?php echo $hackmenu; ?><div><?php echo $linktype; ?></div></a><?php
 		break;
 	case 2:
 		// Use JavaScript "window.open"
 		$options = 'toolbar=no,location=no,status=no,menubar=no,scrollbars=yes,resizable=yes,' . $params->get('window_open');
-			?><a <?php echo $class; ?>href="<?php echo $flink; ?>" onclick="window.open(this.href,'targetWindow','<?php echo $options;?>');return false;" <?php echo $title; ?>><?php echo $hackmenu . $linktype; ?></a><?php
+			?><a <?php echo $class; ?>href="<?php echo $flink; ?>" onclick="window.open(this.href,'targetWindow','<?php echo $options;?>');return false;" <?php echo $title; ?>><?php echo $hackmenu; ?><div><?php echo $linktype; ?></div></a><?php
 		break;
 endswitch;
