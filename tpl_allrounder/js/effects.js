2bd1b707c2cb2846ddbddefb3248549d0062c415
Thomas Hunziker <github@bakual.ch>
2022-02-10 14:41:03 +0100

    Fix JS if no menu is present

--- tpl_allrounder/js/effects.js
+++ tpl_allrounder/js/effects.js
@@ -1,58 +1,61 @@
-document.addEventListener("DOMContentLoaded", function() {
-    // Dropdown menu
-    let entries = document.querySelector("#topmenu").querySelectorAll('ul.nav li');
+document.addEventListener("DOMContentLoaded", function () {
+  // Dropdown menu
+  let topmenu = document.querySelector("#topmenu");
+  if (topmenu) {
+    let entries = topmenu.querySelectorAll('ul.nav li');
     for (let i = 0; i < entries.length; i++) {
-        let entry = entries[i];
-        let mobileIconNode = document.createElement('span');
-        mobileIconNode.className = 'mobile-icon';
-        let link = entry.querySelector('a, span.separator');
-        link.insertBefore(mobileIconNode ,link.firstChild);
-        let ul = entry.querySelector('ul');
-        if (ul){
-            entry.addEventListener('mouseover', function (event) {
-                if (!event.target.classList.contains('mobile-icon')){
-                    entry.querySelector('ul').style.display = 'block';
-                    entry.classList.add('opened');
-                }
-             }, false);
-            entry.addEventListener('mouseout', function (event) {
-                if (!event.target.classList.contains('mobile-icon')) {
-                    entry.querySelector('ul').style.display = 'none';
-                    entry.classList.remove('opened');
-                }
-            }, false);
-            let mobileIcon = entry.querySelector('.mobile-icon');
-            mobileIcon.addEventListener('click', function (event) {
-                if (entry.classList.contains('opened')) {
-                    entry.querySelector('ul').style.display = 'none';
-                    entry.classList.remove('opened');
-                } else {
-                    entry.querySelector('ul').style.display = 'block';
-                    entry.classList.add('opened');
-                }
-                event.preventDefault();
-            }, false);
-        }
+      let entry = entries[i];
+      let mobileIconNode = document.createElement('span');
+      mobileIconNode.className = 'mobile-icon';
+      let link = entry.querySelector('a, span.separator');
+      link.insertBefore(mobileIconNode, link.firstChild);
+      let ul = entry.querySelector('ul');
+      if (ul) {
+        entry.addEventListener('mouseover', function (event) {
+          if (!event.target.classList.contains('mobile-icon')) {
+            entry.querySelector('ul').style.display = 'block';
+            entry.classList.add('opened');
+          }
+        }, false);
+        entry.addEventListener('mouseout', function (event) {
+          if (!event.target.classList.contains('mobile-icon')) {
+            entry.querySelector('ul').style.display = 'none';
+            entry.classList.remove('opened');
+          }
+        }, false);
+        let mobileIcon = entry.querySelector('.mobile-icon');
+        mobileIcon.addEventListener('click', function (event) {
+          if (entry.classList.contains('opened')) {
+            entry.querySelector('ul').style.display = 'none';
+            entry.classList.remove('opened');
+          } else {
+            entry.querySelector('ul').style.display = 'block';
+            entry.classList.add('opened');
+          }
+          event.preventDefault();
+        }, false);
+      }
     }
+  }
 
-    // Article Details
-    let infoTerms = document.querySelectorAll('.article-info-term');
-    for (let i = 0; i < infoTerms.length; i++) {
-        infoTerms[i].addEventListener('click', function (event) {
-            infoTerms[i].parentNode.classList.toggle('opened')
-        });
-    }
+  // Article Details
+  let infoTerms = document.querySelectorAll('.article-info-term');
+  for (let i = 0; i < infoTerms.length; i++) {
+    infoTerms[i].addEventListener('click', function (event) {
+      infoTerms[i].parentNode.classList.toggle('opened')
+    });
+  }
 
 // Toppanel
-    $('a#toppanel-toggle').click(function () {
-        $('#toppanel').slideToggle('middle');
-        return false;
-    });
+  $('a#toppanel-toggle').click(function () {
+    $('#toppanel').slideToggle('middle');
+    return false;
+  });
 
 // Sidepanel
-    $('a#sidepanel-toggle').click(function () {
-        $('#sidepanel').toggle('middle');
-        return false;
-    });
+  $('a#sidepanel-toggle').click(function () {
+    $('#sidepanel').toggle('middle');
+    return false;
+  });
 })
 ;
